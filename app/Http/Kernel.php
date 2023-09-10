adc94ab73f6467dffb832203ba87a79cb19f54d0
Danyel Montazeri <danyel.fcb@gmail.com>
2023-09-10 15:11:02 +0330

    test commit git flow

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -41,6 +41,8 @@ class Kernel extends HttpKernel
         'api' => [
             // \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
             \Illuminate\Routing\Middleware\ThrottleRequests::class.':api',
+            \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
+            'throttle:60,1',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
     ];
